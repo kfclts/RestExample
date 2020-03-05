6d89ede6a83931ae971dbb6c77dd718b83b9f8ff
kfclts <kfclts@gmail.com>
2020-03-05 10:32:57 +0800

    Add CRUD Method

--- src/main/java/space/kfclts/app/ws/ui/controller/UserController.java
+++ src/main/java/space/kfclts/app/ws/ui/controller/UserController.java
@@ -1,5 +1,9 @@
 package space.kfclts.app.ws.ui.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +11,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("users") 	// http://localhost:8000/users
 public class UserController {
 
+	@GetMapping
+	public String getUser() {
+		return "Get User was called";
+	}
+	
+	@PostMapping
+	public String createUser () {
+		return "Create User was called";
+	}
+	
+	@PutMapping
+	public String updateUser() {
+		return "Update User was called";
+	}
+	
+	
+	@DeleteMapping
+	public String deleteUser() {
+		return "Delete User was called";
+	}
 }
