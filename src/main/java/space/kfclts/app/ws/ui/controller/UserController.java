22495e2033c01de575146518ff28c2b2a8e73473
kfclts <kfclts@gmail.com>
2020-03-05 10:28:12 +0800

    Create User Controller Class

---
+++ src/main/java/space/kfclts/app/ws/ui/controller/UserController.java
@@ -0,0 +1,10 @@
+package space.kfclts.app.ws.ui.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("users") 	// http://localhost:8000/users
+public class UserController {
+
+}
