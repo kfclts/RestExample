5b45f60577efc4030d14fda30dbaddb0a4b36aea
kfclts <kfclts@gmail.com>
2020-03-05 16:18:42 +0800

    Handle Exception

--- src/main/java/space/kfclts/app/ws/ui/controller/UserController.java
+++ src/main/java/space/kfclts/app/ws/ui/controller/UserController.java
@@ -39,6 +39,11 @@ public class UserController {
 	@GetMapping(path = "/{userId}", produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
 	public ResponseEntity<UserRest> getUser(@PathVariable String userId) {
 		
+		// Make a exception
+		String firstName = null;
+		int length = firstName.length();
+		
+		
 		if (users.containsKey(userId)) {
 			return new ResponseEntity<>(users.get(userId), HttpStatus.OK);
 		}
