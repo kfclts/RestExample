adbb1324bf79657f17aeeeadf41ffd07b55f3ab4
kfclts <kfclts@gmail.com>
2020-03-05 10:48:46 +0800

    @PathVariable Example

--- src/main/java/space/kfclts/app/ws/ui/controller/UserController.java
+++ src/main/java/space/kfclts/app/ws/ui/controller/UserController.java
@@ -2,6 +2,7 @@ package space.kfclts.app.ws.ui.controller;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,9 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("users") 	// http://localhost:8000/users
 public class UserController {
 
-	@GetMapping
-	public String getUser() {
-		return "Get User was called";
+	@GetMapping(path = "/{userId}")
+	public String getUser(@PathVariable String userId) {
+		return "Get User was called w/ userId = " + userId;
 	}
 	
 	@PostMapping
