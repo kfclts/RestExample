482153d76fc8f5c0de40919fb851f63aa56bce90
kfclts <kfclts@gmail.com>
2020-03-05 12:09:18 +0800

    Add Http Request Body

--- src/main/java/space/kfclts/app/ws/ui/controller/UserController.java
+++ src/main/java/space/kfclts/app/ws/ui/controller/UserController.java
@@ -8,10 +8,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import space.kfclts.app.ws.ui.controller.ui.model.request.UserDetailRequestModel;
 import space.kfclts.app.ws.ui.controller.ui.model.response.UserRest;
 
 @RestController
@@ -36,9 +38,16 @@ public class UserController {
 		return new ResponseEntity<UserRest>(user, HttpStatus.OK);
 	}
 
-	@PostMapping
-	public String createUser() {
-		return "Create User was called";
+	@PostMapping(consumes = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE }, produces = {
+			MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
+	public ResponseEntity<UserRest> createUser(@RequestBody UserDetailRequestModel userDetails) {
+		UserRest user = new UserRest();
+		user.setFirstName(userDetails.getFirstName());
+		user.setLastName(userDetails.getLastName());
+		user.setEmail(userDetails.getEmail());
+//		user.setUserId(userDetails.getPassword());
+		
+		return new ResponseEntity<UserRest>(user, HttpStatus.CREATED);
 	}
 
 	@PutMapping
